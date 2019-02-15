60bd5b9ebb5aaac068660139519dfef0694bbc8b
Tor Saxberg <torsaxberg@gmail.com>
2019-02-15 06:59:35 -0800

    added planned nanodegrees and syllabi

---
+++ courses.py
@@ -0,0 +1,73 @@
+courses taken:
+    Machine Learning Nanodegree
+        Model Evaluation and Validation
+             Predicting Boston Housing Prices
+        Supervised Learning
+            Find Donors for CharityML
+        Unsupervised Learning
+            Creating Customer Segments
+
+    Deep Learning Nanodegree
+    Deep Reinforcement Learning Nanodegree
+    Data Analyst Nanodegree
+    Data Scientist Nanodegree
+        Supervised Learning
+            Find Donors for CharityML with Kaggle
+        Unsupervised Learning
+             Creating Customer Segments
+
+courses to: take
+    Machine Learning Nanodegree
+        Deep Learning
+            Dog Breed Classifier
+        Reinforcement Learning
+            Train a Quadcopter to Fly
+        Project 6: Capstone Proposal
+        Project 7: Capstone Project
+    
+    Deep Learning Nanodegree
+        Neural Networks
+            Predicting Bike-Sharing Patterns
+        Convolutional Neural Networks
+            Dog Breed Classifier
+        Recurrent Neural Networks
+             Generate TV Scripts
+        Generative Adversarial Networks
+            Generate Faces
+        Model Deployment
+            Deploy a Sentiment Analysis Model
+
+    Deep Reinforcement Learning Nanodegree
+        Foundations of Reinforcement Learning
+        Value-Based Methods
+            Navigation
+        Policy-Based Methods
+            Continuous Control
+        Multi-Agent Reinforcement Learning
+            Collaboration and Competition
+
+    Data Analyst Nanodegree
+        Introduction to Data Analysis
+            Investigate a Dataset
+        Practical Statistics
+            Analyze Experiment Results
+        Data Wrangling
+            Wrangle and Analyze Data
+        Data Visualization with Python
+            Communicate Data Findings
+
+    Data Scientist Nanodegree
+        Introduction to Deep Learning
+            Create an Image Classifier
+        Solving Problems with Data Science
+            Write a Data Science Blog Post
+        Software Engineering for Data Scientists
+        Data Engineering for Data Scientists
+            Build Pipelines to Classify Messages with Figure Eight
+        Experiment Design
+        Recommendations
+            Design a Recommendation Engine with IBM
+        Data Science Projects
+            Project 7: Data Science Capstone Project
+                        
+
